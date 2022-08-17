03f486814f6252f91fe955960124c1d26e2ead7e
Björn Friedrichs <bjoern@friedrichs1.de>
2022-08-17 19:53:03 +0100

    Remove web vitals

--- web/app/src/index.js
+++ web/app/src/index.js
@@ -3,13 +3,11 @@ import { hydrate } from 'preact';
 import './index.css';
 import './remirror.css';
 import AppServer from './AppServer';
-import reportWebVitals from './reportWebVitals';
 
 if (process.env.NODE_ENV === 'development') {
   if (module.hot) {
     module.hot.accept();
   }
 }
-
 
 hydrate(<AppServer />, document.getElementById('root'));
