27cfeb7b24296d504154e2a341d0b13739150bf0
Björn Friedrichs <BFriedrichs@users.noreply.github.com>
2023-10-31 09:11:35 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://xn--bjrnf-kua.com',
   output: "server",
   adapter: node({
     mode: "standalone"
