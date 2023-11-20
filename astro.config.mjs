7db81e1f31b04361b87e423b3c4396823495de50
Björn Friedrichs <bjoern@friedrichs1.de>
2023-11-20 18:45:18 +0000

    Remove svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind()],
   site: 'https://bjoernf.com',
   output: "server",
   adapter: node({
